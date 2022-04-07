d2b6c8a146f9e2deec1dd15da4b84a82179e660f
Martin Oliva <martinoliva2013@gmail.com>
2022-04-07 08:55:27 -0300

    Fixed exceptions

--- src/main/java/com/solvd/army/Models/exceptions/NoTimeExplosive.java
+++ src/main/java/com/solvd/army/Models/exceptions/NoTimeExplosive.java
@@ -1,7 +1,7 @@
 package com.solvd.army.Models.exceptions;
 @SuppressWarnings("serial")
 
-public class NoTimeExplosive extends RuntimeException {
+public class NoTimeExplosive extends Exception {
 	
 	public NoTimeExplosive(String errorMessage){
 		super(errorMessage);
--- src/main/java/com/solvd/army/Models/exceptions/WeaponTooHeavy.java
+++ src/main/java/com/solvd/army/Models/exceptions/WeaponTooHeavy.java
@@ -1,7 +1,7 @@
 package com.solvd.army.Models.exceptions;
 @SuppressWarnings("serial")
 
-public class WeaponTooHeavy extends RuntimeException {
+public class WeaponTooHeavy extends Exception {
 	
 	public WeaponTooHeavy(String errorMessage) {
 		super(errorMessage);
